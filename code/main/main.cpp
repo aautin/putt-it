8988efb8f6a080ec6a229891ff14781ef8c587a7
aautin <alexandreautin.aa@gmail.com>
2025-12-30 00:07:37 +0100

    Simple cahhhnegs

--- code/main/main.cpp
+++ code/main/main.cpp
@@ -9,65 +9,57 @@
 #include <conversion.h>
 
 #define PRIMARY_DISPLAY_INDEX 0
-#define ORIGINAL_WIDTH        1000.f
-#define ORIGINAL_HEIGHT       1500.f
-#define WINDOW_OFFSET 0.9f
+#define WINDOW_OFFSET         0.9f
 
 using namespace core::conversion::sdl;
 
-SDL_FPoint getWindowSize(SDL_FRect& display)
-{
-    if (display.w / display.h < ORIGINAL_WIDTH / ORIGINAL_HEIGHT)
-    {
-        return { display.w * WINDOW_OFFSET, display.w * (ORIGINAL_HEIGHT / ORIGINAL_WIDTH) * WINDOW_OFFSET };
-    }
-    else
-    {
-        return { display.h * (ORIGINAL_WIDTH / ORIGINAL_HEIGHT) * WINDOW_OFFSET, display.h * WINDOW_OFFSET };
-    }
-}
-
 int main(int argc, char* argv[])
 {
-    if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
-    {
-        std::cerr << "SDL_Init: " << SDL_GetError() << std::endl;
-        return EXIT_FAILURE;
-    }
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
+	{
+		std::cerr << "SDL_Init: " << SDL_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
 
-    IMG_Init(IMG_INIT_PNG);
-    SDL_Surface* icon = IMG_Load("assets/icon.png");
-    if (icon == NULL)
-    {
-        std::cerr << "IMG_Load: " << IMG_GetError() << std::endl;
-        return EXIT_FAILURE;
-    }
+	SDL_Rect display;
+	if (SDL_GetDisplayUsableBounds(PRIMARY_DISPLAY_INDEX, &display) != 0)
+	{
+		std::cerr << "SDL_GetDisplayUsableBounds: " << SDL_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	auto *window = SDL_CreateWindow("Putt-It",
+		SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+		800, 800,
+		0);
+	
+	IMG_Init(IMG_INIT_PNG);
+	SDL_Surface* icon = IMG_Load("assets/icon.png");
+	if (icon == NULL)
+	{
+		std::cerr << "IMG_Load: " << IMG_GetError() << std::endl;
+		return EXIT_FAILURE;
+	}
+	
+	SDL_SetWindowIcon(window, icon);
+	SDL_FreeSurface(icon);
 
-    SDL_Rect displayRectangle;
-    if (SDL_GetDisplayUsableBounds(PRIMARY_DISPLAY_INDEX, &displayRectangle) != 0)
-    {
-        std::cerr << "SDL_GetDisplayUsableBounds: " << SDL_GetError() << std::endl;
-        return EXIT_FAILURE;
-    }
-    
-    auto  size = getWindowSize(FRect(displayRectangle));
-    auto *window = SDL_CreateWindow("Putt-It",
-        SDL_WINDOWPOS_CENTERED,
-        SDL_WINDOWPOS_CENTERED,
-        size.x, size.y,
-        SDL_WINDOW_MOUSE_GRABBED);
-
-    SDL_SetWindowIcon(window, icon);
-    while (true)
-    {
-        //
-        // Inputs handling
-        //
-        SDL_Event Event;
-        while (SDL_PollEvent(&Event) != 0) {
-        
-        }
-    }
-
-    SDL_Quit();
+	//
+	// Main game loop
+	//
+	while (true)
+	{
+		//
+		// Inputs handling
+		//
+		SDL_Event event;
+		while (SDL_PollEvent(&event) != 0)
+		{
+			if (event.type == SDL_EventType::SDL_WINDOWEVENT
+				&& event.window.event == SDL_WindowEventID::SDL_WINDOWEVENT_CLOSE)
+			{
+				SDL_Quit();
+			}
+		}
+	}
 }
